77b4cbf29a2ed012fa084d5109c6d8d5ea17b96e
david <david.anderson1993@gmail.com>
2022-07-01 15:40:48 -0500

    added functionality to update and add artists working on delete now

--- artistManagement_Frontend/artistmanagementapp/src/app/app.component.ts
+++ artistManagement_Frontend/artistmanagementapp/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Artist } from './artist';
 import { ArtistService } from './artist.service';
 
@@ -10,7 +11,7 @@ import { ArtistService } from './artist.service';
 })
 export class AppComponent implements OnInit {
   public artists: Artist[];
-  public editArtists: Artist;
+  public editArtist: Artist;
   public deleteArtists: Artist;
 
   constructor(private artistService: ArtistService) {}
@@ -28,5 +29,51 @@ export class AppComponent implements OnInit {
         alert(error.message);
       }
     );
+  }
+
+  public onAddArtist(addForm: NgForm): void {
+    document.getElementById('add-artist-form').click();
+    this.artistService.addArtist(addForm.value).subscribe(
+      (res: Artist) => {
+        console.log(res);
+        this.getArtists();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onUpdateArtist(artist: Artist): void {
+    this.artistService.updateArtist(artist).subscribe(
+      (res: Artist) => {
+        console.log(res);
+        this.getArtists();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onOpenModal(artist: Artist, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    // checking the button mode
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addArtistModal');
+    }
+    if (mode === 'edit') {
+      this.editArtist = artist;
+      button.setAttribute('data-target', '#updateArtistModal');
+    }
+    if (mode === 'delete') {
+      button.setAttribute('data-target', '#deleteArtistModal');
+    }
+    container.appendChild(button);
+    button.click();
   }
 }
